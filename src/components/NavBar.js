477ead7876f619b7988e1cf2e386a1f3741bb84d
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-01-11 22:56:50 +0300

    commit tree 0976ff58faf2774b9308d1f05aef058edf4f35b8

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  let myLinks = links.map((link) => <a key={link} href={"#"+link} >{link}</a>)
+  return <nav>
+    {myLinks}
+   
+  </nav>;
 }
 
 export default NavBar;
